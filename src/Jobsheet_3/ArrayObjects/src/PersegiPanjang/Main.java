588fc00f5983ce2f97c0c70e995a7dd93fcbb66e
ayleenrq <qisthyayleen@gmail.com>
2024-02-28 00:22:11 +0700

    P3_Percobaan 2_Pertanyaan 4_07

---
+++ src/Jobsheet_3/ArrayObjects/src/PersegiPanjang/Main.java
@@ -0,0 +1,28 @@
+package Jobsheet_3.ArrayObjects.src.PersegiPanjang;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ayleen07 = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang array: ");
+        int length = ayleen07.nextInt();
+
+        PersegiPanjang[] ppArray = new PersegiPanjang[length];
+
+        for (int i = 0; i < ppArray.length; i++) {
+            ppArray[i] = new PersegiPanjang();
+            System.out.println("Persegi panjang ke-" + i);
+            System.out.print("Masukkan panjang : ");
+            ppArray[i].panjang = ayleen07.nextInt();
+            System.out.print("Masukkan lenar : ");
+            ppArray[i].lebar = ayleen07.nextInt();
+        }
+        System.out.println();
+
+        for (int i = 0; i < ppArray.length; i++) {
+            System.out.println("Persegi Panjang ke-" + i);
+            System.out.println("Panjang : " + ppArray[i].panjang + ", lebar : " + ppArray[i].lebar);
+        }
+    }
+}
---
+++ src/Jobsheet_3/ArrayObjects/src/PersegiPanjang/PersegiPanjang.java
@@ -0,0 +1,6 @@
+package Jobsheet_3.ArrayObjects.src.PersegiPanjang;
+
+public class PersegiPanjang {
+    public int panjang;
+    public int lebar;
+}
