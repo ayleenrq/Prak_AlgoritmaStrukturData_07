6be1729b384add801960308bb124dc4ca6359a8e
ayleenrq <qisthyayleen@gmail.com>
2024-02-28 01:42:11 +0700

    P3_Percobaan 3_Pertanyaan 5_07

--- src/Jobsheet_3/ArrayObjects/src/Segitiga/Main.java
+++ src/Jobsheet_3/ArrayObjects/src/Segitiga/Main.java
@@ -8,5 +8,12 @@ public class Main {
         sgArray[1] = new Segitiga(20, 10);
         sgArray[2] = new Segitiga(15, 6);
         sgArray[3] = new Segitiga(25, 10);
+
+        for (int i = 0; i < sgArray.length; i++) {
+            System.out.println("Segitiga ke-" + i);
+            System.out.println("Luas: " + sgArray[i].hitungLuas());
+            System.out.println("Keliling: " + sgArray[i].hitungKeliling()); 
+            System.out.println();
+        }
     }
 }
