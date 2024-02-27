0c06c0e06f224298cfb61962e96dbc7c95de5cd5
ayleenrq <qisthyayleen@gmail.com>
2024-02-28 01:04:22 +0700

    P3_Percobaan 3_Pertanyaan 2_07

---
+++ src/Jobsheet_3/ArrayObjects/src/Segitiga/Segitiga.java
@@ -0,0 +1,11 @@
+package Jobsheet_3.ArrayObjects.src.Segitiga;
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga(int a, int t) {
+        alas = t;
+        tinggi = t;
+    }
+}
