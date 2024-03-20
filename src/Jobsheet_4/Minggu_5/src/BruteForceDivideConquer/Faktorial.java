f1daaeb790c7e3a6033de21a3fa07f2726fd90fd
ayleenrq <qisthyayleen@gmail.com>
2024-03-20 19:52:12 +0700

    P4_Percobaan 1_07

---
+++ src/Jobsheet_4/Minggu_5/src/BruteForceDivideConquer/Faktorial.java
@@ -0,0 +1,22 @@
+package Jobsheet_4.Minggu_5.src.BruteForceDivideConquer;
+
+public class Faktorial {
+    public int nilai;
+
+    int faktorialBF(int n) {
+        int fakto = 1;
+        for (int i = 1; i <= n; i++) {
+            fakto *= i;
+        }
+        return fakto;
+    }
+    int faktorialDC(int n) {
+        if (n == 1) {
+            return 1;
+        } 
+        else {
+            int fakto = n * faktorialDC(n - 1);
+            return fakto;
+        }
+    }
+}
---
+++ src/Jobsheet_4/Minggu_5/src/BruteForceDivideConquer/MainFaktorial.java
@@ -0,0 +1,27 @@
+package Jobsheet_4.Minggu_5.src.BruteForceDivideConquer;
+import java.util.Scanner;
+
+public class MainFaktorial {
+    public static void main(String[] args) {
+        Scanner ayleen07 = new Scanner(System.in);
+
+        System.out.println("--------------------------");
+        System.out.print("Masukkan jumlah elemen : ");
+        int ijml = ayleen07.nextInt();
+
+        Faktorial[] fk = new Faktorial[10];
+        for (int i = 0; i < ijml; i++) {
+            fk[i] = new Faktorial();
+            System.out.print("Masukkan nilai data ke-" + (i+1) + " : ");
+            fk[i].nilai = ayleen07.nextInt();
+        }
+        System.out.println("HASIL - BRUTE FORCE");
+        for (int i = 0; i < ijml; i++) {
+            System.out.println("Hasil perhitungan faktorial menggunakan Brute Force adalah " + fk[i].faktorialBF(fk[i].nilai));
+        }
+        System.out.println("HASIL - DIVIDE AND CONQUER");
+        for (int i = 0; i < ijml; i++) {
+            System.out.println("Hasil perhitungan faktorial menggunakan Divide and Conquer adalah " + fk[i].faktorialDC(fk[i].nilai));
+        }
+    }
+}
