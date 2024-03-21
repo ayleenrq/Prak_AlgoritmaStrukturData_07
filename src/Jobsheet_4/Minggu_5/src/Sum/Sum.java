c246a42be1530a05778e1c736f7ba97b6d56ffe7
ayleenrq <qisthyayleen@gmail.com>
2024-03-21 19:33:38 +0700

    P4_Percobaan 3_07 FIX

--- src/Jobsheet_4/Minggu_5/src/Sum/Sum.java
+++ src/Jobsheet_4/Minggu_5/src/Sum/Sum.java
@@ -19,11 +19,11 @@ public class Sum {
         if (l == r) {
             return arr[l];
         } else if (l < r) {
-            int mid = (l + r) / 2; 
-            double lsum = totalDC(arr, l, mid);
+            int mid = (l + r) / 2;
+            double lsum = totalDC(arr, l, mid - 1);
             double rsum = totalDC(arr, mid + 1, r);
-            return lsum + rsum;
+            return lsum + rsum + arr[mid];
         }
-        return 0; 
+        return 0;
     }
 }
