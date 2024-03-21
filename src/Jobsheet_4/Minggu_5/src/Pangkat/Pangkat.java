8018d3996397987b8325572158ea4858cfe0b7ba
ayleenrq <qisthyayleen@gmail.com>
2024-03-21 15:21:30 +0700

    P4_Percobaan 2_07 FIX

--- src/Jobsheet_4/Minggu_5/src/Pangkat/Pangkat.java
+++ src/Jobsheet_4/Minggu_5/src/Pangkat/Pangkat.java
@@ -4,15 +4,15 @@ public class Pangkat {
     public int nilai, pangkat;
 
     int pangkatBF(int a, int n) {
-        int hasil = 0;
-        for (int i = 0; i <= n; i++) {
-            hasil = a * a;
+        int hasil = 1;
+        for (int i = 1; i <= n; i++) {
+            hasil *= a;
         }
         return hasil;
     }
     int pangkatDC(int a, int n) {
-        if (n == 1) {
-            return a;
+        if (n == 0) {
+            return 1;
         } else {
             if (n % 2 == 1) {
                 return (pangkatDC(a, n/2) * pangkatDC(a, n/2) * a);
