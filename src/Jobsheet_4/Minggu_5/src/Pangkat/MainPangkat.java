b26441b2294159dd665dbcca7231d2b7722069e7
ayleenrq <qisthyayleen@gmail.com>
2024-03-21 15:46:38 +0700

    P4_Percobaan 2_Pertanyaan 4_07

--- src/Jobsheet_4/Minggu_5/src/Pangkat/MainPangkat.java
+++ src/Jobsheet_4/Minggu_5/src/Pangkat/MainPangkat.java
@@ -17,13 +17,27 @@ public class MainPangkat {
             int pangkat = ayleen07.nextInt();
             png[i] = new Pangkat(nilai, pangkat);
         }
-        System.out.println("HASIL PANGKAT - BRUTE FORCE");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
-        }
-        System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+        System.out.println("Pilih method yang ingin dijalankan :");
+        System.out.println("1. BRUTE FORCE");
+        System.out.println("2. DIVIDE AND CONQUER");
+        System.out.print("Pilih method : ");
+        int menu = ayleen07.nextInt();
+
+        switch (menu) {
+            case 1:
+                System.out.println("HASIL PANGKAT - BRUTE FORCE");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            case 2:
+                System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            default:
+                System.out.println("Pilihan menu tidak valid!");
         }
     }
 }
