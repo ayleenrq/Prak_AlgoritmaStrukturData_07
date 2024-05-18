ecd242a62f0984e282f1b77823deeda7829538b9
ayleenrq <qisthyayleen@gmail.com>
2024-05-18 18:15:14 +0700

    P12_Percobaan 2_07_DoubleLinkedList

--- src/Jobsheet_10/DoubleLinkedList/DoubleLinkedListMain.java
+++ src/Jobsheet_10/DoubleLinkedList/DoubleLinkedListMain.java
@@ -3,17 +3,19 @@ package Jobsheet_10.DoubleLinkedList;
 public class DoubleLinkedListMain {
     public static void main(String[] args) throws Exception {
         DoubleLinkedList dll = new DoubleLinkedList();
-        
+
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
         dll.print();
         System.out.println("Size : " + dll.size());
         System.out.println("============================================");
-        dll.addFirst(3);
-        dll.addLast(4);
-        dll.addFirst(7);
+        dll.removeFirst();
         dll.print();
         System.out.println("Size : " + dll.size());
         System.out.println("============================================");
-        dll.add(40, 1);
+        dll.removeLast();
         dll.print();
         System.out.println("Size : " + dll.size());
         System.out.println("============================================");
